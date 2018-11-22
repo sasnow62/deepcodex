65dfff901d2e695ef8a612b431038f459af0a843
Samuel Snow <sasnow62@gmail.com>
2018-11-22 17:03:59 +0700

    init

---
+++ muse/data.js
@@ -0,0 +1,125 @@
+var data = {
+  india: {
+    panicker: [
+      "https://www.sahapedia.org/sites/default/files/Paniker90.jpg",
+      "http://www.artnet.com/WebServices/images/ll00072lld38SGFgUNECfDrCWvaHBOcEyMC/k.c.s.-paniker-words-and-symbols.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker78.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker7.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker84.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker3.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker80.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker99.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker92.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker77.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker100.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker95.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker88.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker89.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker1.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker75.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker4.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker74.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker98.jpg",
+      "https://www.sahapedia.org/sites/default/files/Paniker94.jpg"
+    ],
+    arch: [
+      "https://www.sahapedia.org/sites/default/files/Garima_07.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Manikarnika_Cremation_Ghat%2C_Varanasi.jpg/1200px-Manikarnika_Cremation_Ghat%2C_Varanasi.jpg",
+      "https://cdn1.goibibo.com/t_tg_fs/uttar-pradesh-varanasi-148915950084-orijgp.jpg",
+      "https://i.ytimg.com/vi/rDsOi3MByCI/maxresdefault.jpg",
+      "https://images.thrillophilia.com/image/upload/s--BQRzOdYb--/c_fill,f_auto,fl_strip_profile,h_775,q_auto,w_1600/v1/images/photos/000/013/479/original/Varanasi_Tour_Packages-Wikipedia.jpg.jpg?1458177849",
+      "http://cdn.historydiscussion.net/wp-content/uploads/2015/07/india-architecture-and-nature-new-indian-temples-indo-islamic31.jpg",
+      "http://squareone.blog/wp-content/uploads/2017/09/india-1463864_1920-e1506074833454-1920x768.jpg",
+      "http://goheritagerun.com.s3-us-west-1.amazonaws.com/wp-content/uploads/2015/08/26051619/Divine-Dravidian-Architecture.jpg",
+      "https://s3.ap-south-1.amazonaws.com/zoom-blog-image/2016/04/Ellora_cave16_003.jpg"
+    ],
+    misc: [
+      "https://pbs.twimg.com/media/Dpv1IqxX4AEx8tz.jpg",
+      "https://i.etsystatic.com/8113517/r/il/9be986/1385501103/il_570xN.1385501103_9oc4.jpg",
+      "https://images.fineartamerica.com/images-medium-large-5/goddess-kali-chinnamastka-tantra-tantric-artwork-paper-painting-miniature-painting-art-gallery-a-k-mundhra.jpg",
+      "https://www.thebetterindia.com/wp-content/uploads/2015/12/10.jpg"
+    ]
+  },
+  future: {
+    gonzalez: [
+      "https://rickwayneauthor.files.wordpress.com/2018/05/josan-gonzalez-cochepolice7nimationv3_by_f1x_2-d9up5xp.gif?w=736",
+      "https://cdn1.vox-cdn.com/uploads/chorus_asset/file/6274069/a7315531563275.565618dfcf2e0.0.jpg",
+      "https://www.neondystopia.com/wp-content/uploads/2016/03/thefuture7.jpg",
+      "https://cdn.vox-cdn.com/thumbor/ehHS8Hf9rb-NenAezV3AQ10P4g8=/0x24:1200x824/1280x854/cdn.vox-cdn.com/uploads/chorus_image/image/49212331/d153dd31563275.565618dfca873.0.0.jpg",
+      "https://cdna.artstation.com/p/assets/images/images/007/510/490/large/josan-gonzalez-overwatchmasterfilev24.jpg?1506624902",
+      "https://cdn.shopify.com/s/files/1/0185/4636/products/Raid-8piece-set_2000x.png?v=1481140170",
+      "https://i.redd.it/5v8m16j7vde11.jpg",
+      "http://www.graphicine.com/wp-content/uploads/2017/12/15_josan_gonzalez_graphicine.jpg",
+      "https://i.pinimg.com/originals/f3/59/54/f3595461a56ba4e8d683424cce7f823f.jpg",
+      "http://www.artfutura.org/v3/wp-content/uploads/01-The-Future-is-Now-Josan-3.jpg",
+      "https://i.redd.it/w6b51x0r79u01.jpg"
+    ]
+  },
+  ksbd: [
+    "https://killsixbilliondemons.com/wp-content/uploads/2015/08/Thronerevisited.jpg",
+    "https://killsixbilliondemons.com/wp-content/uploads/2015/04/KSBD89.jpg",
+    "https://i.redd.it/bhy4zgmlz2z01.jpg",
+    "https://killsixbilliondemons.com/wp-content/uploads/2015/12/WON40.jpg",
+    "https://i.pinimg.com/originals/30/1d/77/301d77c2e46b4129a7ad99c3a77325bc.png",
+    "https://i.pinimg.com/originals/ee/06/62/ee0662bd9219500f143fb2c064c3e8d1.jpg",
+    "http://de8ib6lw8p0gm.cloudfront.net/wp-content/uploads/2014/08/KSBD55color.jpg",
+    "http://i.imgur.com/ICmuPtk.jpg"
+  ],
+  alchemy: [
+    "https://www.alchemywebsite.com/images/A016.jpg",
+    "https://www.alchemywebsite.com/images/A011.jpg",
+    "https://www.alchemywebsite.com/images/A022.jpg",
+    "https://www.alchemywebsite.com/images/A027.jpg",
+    "https://www.alchemywebsite.com/images/ED01.jpg",
+    "https://www.alchemywebsite.com/images/A005.jpg",
+    "https://www.alchemywebsite.com/images/FR06.jpg",
+    "https://divjy4ye5sj3p.cloudfront.net/artists/354854/artwork/3477443_original-3.jpg",
+    "https://www.alchemywebsite.com/images/DCS14.jpg",
+    "https://www.alchemywebsite.com/images/DCS24.jpg",
+    "https://www.alchemywebsite.com/images/DCS26.jpg",
+    "https://www.alchemywebsite.com/images/DCS13.jpg",
+    "https://i.ytimg.com/vi/xKfYeSFgNHw/maxresdefault.jpg",
+    "http://www.getty.edu/research/exhibitions_events/exhibitions/alchemy/images/0_480_RipleyScroll.jpg",
+  ],
+  champa: {
+    mainframe: [
+      "worlds/champa/champa-babel.png",
+      "worlds/champa/init.svg"
+    ],
+    temples: [
+      "worlds/champa/4711725238_86e2edf1fb_z.jpg",
+      "worlds/champa/7cba19d175c58368d3f1eea949698ab3.jpg",
+      "worlds/champa/My-Son-Linga-and-ruins.jpg",
+      "worlds/champa/8277593934_038a74d516_k.jpg",
+      "worlds/champa/duong-long-cham-towers-cha-ban-cham-area-4.jpg",
+      "worlds/champa/8281817082_501f6f084e_k.jpg",
+      "worlds/champa/architecture-phu-yen.jpg",
+      "worlds/champa/Champa-Po-Nagar-Nha-Trang-banner-1.jpg"
+    ],
+    figures: [
+      "https://upload.wikimedia.org/wikipedia/vi/e/ed/%C4%90%C3%A0i_th%E1%BB%9D_Ph%E1%BA%ADt%2C_ph%C3%A1t_hi%E1%BB%87n_t%E1%BA%A1i_%C4%90%E1%BB%93ng_D%C6%B0%C6%A1ng.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/7/73/Pedestal%2C_Buddha%2C_Bodhisattva%2C_and_Monk%2C_view_4%2C_Dong_Duang%2C_late_9th_to_early_10th_century%2C_Quang_Nam_-_Museum_of_Cham_Sculpture_-_Danang%2C_Vietnam_-_DSC01795.JPG",
+      "https://upload.wikimedia.org/wikipedia/vi/b/be/P1015576.JPG",
+      "http://dantricdn.com/mZ4CmU0ghSWg7wdoD7ro/Image/2011/5_dd79c.JPG",
+      "worlds/champa/449px-Shiva_Kosa_from_Champa.jpg",
+      "worlds/champa/Ganesha,_Vietnam,_Champa_kingdom,_9th-10th_century_AD,_stone_-_Middlebury_College_Museum_of_Art_-_Middlebury,_VT_-_DSC08236.jpg",
+      "worlds/champa/DancingShiva10thc.jpg",
+      "worlds/champa/vietnam-champa-male-figure.jpg",
+      "worlds/champa/Elephant_on_bas_relief.jpg",
+      "worlds/champa/Male_dancer_My_Son_E1_pedestal.jpg",
+      "worlds/champa/Master_and_Disciple.jpg",
+      "worlds/champa/Musician_on_pedestal_MySonE1.jpg",
+      "worlds/champa/Vishnu_Brahma_My_Son_E1.jpg"
+    ]
+  },
+  // unused
+  laprisamata: [
+    "https://payload423.cargocollective.com/1/2/95609/10776551/CancindeMuerto_a72_1900_1000.jpg",
+    "https://payload.cargocollective.com/1/2/95609/1579737/denoche_72.jpg",
+    "https://payload.cargocollective.com/1/2/95609/1361084/panconstrucciontxtrz_WEB_o.jpg",
+    "https://payload562.cargocollective.com/1/2/95609/13543254/Psymurai_laprisamata_1_1000.jpg",
+    "https://payload562.cargocollective.com/1/2/95609/13543254/Duelo_1online_digitalart_Laprisamata_1000.jpg",
+    "https://payload6.cargocollective.com/1/2/95609/2436997/WEBfuerza_9.jpg",
+    "https://payload528.cargocollective.com/1/2/95609/12869690/Guerrera2017online_1000.jpg"
+  ]
+}
---
+++ muse/system.js
@@ -0,0 +1,158 @@
+// DeepCodex Muse System
+
+// Router Init - Source: http://joakim.beng.se/blog/posts/a-javascript-router-in-20-lines.html
+var routes = {}; function route(path, templateId, controller) {
+  routes[path] = {templateId: templateId, controller: controller};
+}
+
+// Routes
+route('/', 'mainframe', function () {
+  this.title = 'Muse Mainframe'
+  this.body = 'Linked Codex of various artists/styles/ideas who inspire my artworks'
+  this.worlds = {
+    india: {
+      link: location.pathname.match(/.*(?=\/)/i)[0] + '/#/india',
+      img: 'worlds/india/tn.jpg'
+    },
+    future: {
+      link: location.pathname.match(/.*(?=\/)/i)[0] + '/#/future',
+      img: 'worlds/future.gif'
+    },
+    ksbd: {
+      link: location.pathname.match(/.*(?=\/)/i)[0] + '/#/ksbd',
+      img: 'worlds/ksbd.jpg'
+    },
+    alchemy: {
+      link: location.pathname.match(/.*(?=\/)/i)[0] + '/#/alchemy',
+      img: 'worlds/alchemy.jpg'
+    },
+    champa: {
+      link: location.pathname.match(/.*(?=\/)/i)[0] + '/#/champa',
+      img: 'worlds/champa/champa.jpg'
+    }
+  }
+});
+
+route('/india', 'artboard', function () {
+  this.title = 'India'
+  this.body = 'Sources: <a target="_blank" href="https://www.sahapedia.org/">Sahapedia</a> '
+  this.worlds = {
+    'K.C.S. Panicker': {
+      link: location.pathname.match(/.*(?=\/)/i)[0] + '#/india/panicker',
+      img: 'worlds/india/panicker.jpg'
+    },
+    'Architecture': {
+      link: location.pathname.match(/.*(?=\/)/i)[0] + '#/india/arch',
+      img: 'worlds/india/arch.jpg'
+    }
+  }
+  this.imgs = showcase('india')
+  // "https://cdn.shopify.com/s/files/1/0969/9128/products/minia_7_54294e38-c036-4a48-afdb-0840fd97c5dc.jpg?v=1518871179"
+});
+route('/india/panicker', 'artboard', function () {
+  this.title = 'K.C.S Panicker'
+  this.body = 'A metaphysical and abstract painter from the 60s and 70s that drew on ancient sources. '
+            + '<a target="_blank" href="https://en.wikipedia.org/wiki/K._C._S._Paniker">Wiki</a>'
+  this.worlds = 'nil'
+  this.imgs = data.india.panicker
+})
+route('/india/arch', 'artboard', function () {
+  this.title = 'Architecture'
+  this.body = 'A sampling of the architecture of India'
+  this.worlds = 'nil'
+  this.imgs = data.india.arch
+});
+
+route('/future', 'artboard', function () {
+  this.title = 'The Future Is Now'
+  this.body = 'Art by <a href="https://ixcitadel.com/">Josan Gonzalez</a>'
+  this.worlds = 'nil'
+  this.imgs = data.future.gonzalez
+});
+route('/ksbd', 'artboard', function () {
+  this.title = 'Kill Six Billion Demons'
+  this.body = 'Art by <a href="https://killsixbilliondemons.com">Abbadon</a>'
+  this.worlds = 'nil'
+  this.imgs = data.ksbd
+});
+route('/alchemy', 'artboard', function () {
+  this.title = 'Collection of Alchemical Drawings'
+  this.body = 'Guide to <a href="https://ultraculture.org/blog/2015/11/13/psychonaut-field-manual/">Chaos Magick</a>'
+  this.worlds = 'nil'
+  this.imgs = data.alchemy
+});
+
+route('/champa', 'artboard', function () {
+  this.title = 'Champa'
+  this.body = 'Inspiration for my Painting at Longson Mui Ne'
+  this.worlds = {
+    'temples': {
+      link: location.pathname.match(/.*(?=\/)/i)[0] + '#/champa/temples',
+      img: 'worlds/champa/temples.jpg'
+    },
+    'figures': {
+      link: location.pathname.match(/.*(?=\/)/i)[0] + '#/champa/figures',
+      img: 'worlds/champa/figures.jpg'
+    }
+  }
+  this.imgs = data.champa.mainframe
+});
+route('/champa/temples', 'artboard', function () {
+  this.title = 'Temples'
+  this.body = ''
+  this.worlds = 'nil'
+  this.imgs = data.champa.temples
+})
+route('/champa/figures', 'artboard', function () {
+  this.title = 'Figures'
+  this.body = ''
+  this.worlds = 'nil'
+  this.imgs = data.champa.figures
+})
+
+var el = null; function router () {
+  el = el || document.getElementById('view');
+  var url = location.hash.slice(1) || '/';
+  var route = routes[url];
+  if (el && route.controller) { el.innerHTML = tmpl(route.templateId, new route.controller()) }
+}
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);
+
+(function(){ // Simple JavaScript Templating by John Resig - https://johnresig.com/ - MIT Licensed
+  var cache = {};
+  this.tmpl = function tmpl(str, data){
+    var fn = !/\W/.test(str) ? cache[str] = cache[str] || tmpl(document.getElementById(str).innerHTML) :
+      new Function("obj", "var p=[], print = function() { p.push.apply(p, arguments) };with(obj){p.push('" + str
+        .replace(/[\r\t\n]/g, " ")
+        .split("<%").join("\t")
+        .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+        .replace(/\t=(.*?)%>/g, "',$1,'")
+        .split("\t").join("');")
+        .split("%>").join("p.push('")
+        .split("\r").join("\\'")
+      + "');}return p.join('');");
+    return data ? fn( data ) : fn;
+  };
+})();
+
+// Helper functions
+function showcase(world) {
+  imgs = []
+  for (var i = 0; i < Object.keys(data[world.toString()]).length; i++) {
+    imgs.push.apply(imgs, data[world.toString()][Object.keys(data.india)[i]])
+  }
+  return shuffle(imgs).slice(0, 10)
+}
+function capitalize(word) {
+  return word.replace(/^\w/, function (chr) { return chr.toUpperCase() })
+}
+function shuffle(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+  return array
+}
